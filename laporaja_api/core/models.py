fdfd5ba99784ce52cc0ca60af52b944014e0c2e0
Shanssea <shaniae.astagina@gmail.com>
2021-06-01 16:30:43 +0700

    generate unique image upload name

--- laporaja_api/core/models.py
+++ laporaja_api/core/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+import uuid
+import os
+
+def get_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join('img', filename)
 
 class User(models.Model):
     name = models.CharField(max_length=100)
@@ -6,7 +13,7 @@ class User(models.Model):
 
 class Report(models.Model):
     location_name = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='img', default=None, blank=True, null=True)
+    image = models.ImageField(upload_to=get_file_path, default=None, blank=True, null=True)
     notes = models.CharField(max_length=1000, default=None, blank=True, null=True)
     lat = models.DecimalField(max_digits=9, decimal_places=6, default=-6.200000)
     long = models.DecimalField(max_digits=9, decimal_places=6, default=106.816666)
