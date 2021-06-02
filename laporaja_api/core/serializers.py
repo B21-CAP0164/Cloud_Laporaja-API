16d46180924474c29ef12385f2268c3c60be54fa
Shanssea <shaniae.astagina@gmail.com>
2021-06-02 16:02:30 +0700

    convert id to string

--- laporaja_api/core/serializers.py
+++ laporaja_api/core/serializers.py
@@ -4,16 +4,32 @@ from .models import Report, User
 
 
 class ReportListSerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        """ Override `to_representation` method """
+        repr = super().to_representation(instance)
+        repr['id'] = str(repr['id'])  
+        return repr
+
     class Meta:
         model = Report
         fields = ['id', 'image', 'location_name', 'user_id']
 
 class ReportDetailSerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        """ Override `to_representation` method """
+        repr = super().to_representation(instance)
+        repr['id'] = str(repr['id'])  
+        return repr
     class Meta:
         model = Report
         fields = '__all__'
 
 class UserSerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        """ Override `to_representation` method """
+        repr = super().to_representation(instance)
+        repr['id'] = str(repr['id'])  
+        return repr
     class Meta:
         model = User
         fields = '__all__'
