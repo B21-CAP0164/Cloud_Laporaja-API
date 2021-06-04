b0b1268b5012157243bcfa9888048fc853020ec7
Shanssea <shaniae.astagina@gmail.com>
2021-06-04 14:22:29 +0000

    alter dalete damage type and alter damage severity

--- laporaja_api/core/models.py
+++ laporaja_api/core/models.py
@@ -16,11 +16,10 @@ class User(models.Model):
 
 class Report(models.Model):
     location_name = models.CharField(max_length=200)
-    image = models.ImageField(upload_to=get_file_path, default=None, blank=True, null=True)
+    image = models.TextField(default=None, blank=True, null=True)
     notes = models.CharField(max_length=1000, default=None, blank=True, null=True)
     lat = models.DecimalField(max_digits=9, decimal_places=6, default=-6.200000)
     long = models.DecimalField(max_digits=9, decimal_places=6, default=106.816666)
-    damage_type = models.CharField(max_length=100)
-    damage_severity = models.CharField(max_length=20)
+    damage_severity = models.CharField(max_length=100, default=None, blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateField(auto_now_add=True)
